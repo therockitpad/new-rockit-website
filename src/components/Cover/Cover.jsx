c82f013ec2f129e6ed55bdc59f16cd1bd7156cb6
Archit Bhonsle <abhonsle2000@gmail.com>
2020-08-26 07:53:51 +0530

    Hero image

---
+++ src/components/Cover/Cover.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './Cover.css';
+
+const Cover = () => <div className="hero-image" />;
+
+export default Cover;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
-import React from "react"
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+import Cover from '../components/Cover/Cover';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
-  return <div>Hello world!</div>
+	return (
+		<div>
+			<Helmet>
+				<title>RockIT | Digital Brand Management</title>
+			</Helmet>
+			<Cover />
+		</div>
+	);
 }
