d6e725e7e93d7508efdede30b534f93d16fd8b9d
Archit Bhonsle <abhonsle2000@gmail.com>
2020-09-03 14:07:59 +0530

    coordinating the carousels and cleaning up the code

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Carousel, Container } from 'react-bootstrap';
 
 import image1 from './images/features-1.jpg';
@@ -9,7 +9,40 @@ import './Services.css';
 
 const images = [ image1, image2, image3, image4 ];
 
+const content = [
+	{
+		header: 'Custom Website',
+		title: 'Design and build a unique website.',
+		desc:
+			'We can create a website from scratch, or improve an existing website. We can also work with templates and sites such as wordpress and wix.'
+	},
+	{
+		header: 'Social Media Management',
+		title: 'Expand your outreach through social media.',
+		desc:
+			'We can manage multiple social media accounts, handling everything from account setup to continual maintenance. We will post content daily and will create graphic content and videos on a monthly basis.'
+	},
+	{
+		header: 'Digital Marketing',
+		title: 'Vast array of tools and knowledge to help you achieve your marketing needs.',
+		desc:
+			'Search Engine Optimization, Email campaigns, advertising, are just a few options available in our marketing arsenal. We can create innovative designs and content that will catch the eye of your audience, and will increase your brand engagement.'
+	},
+	{
+		header: 'Graphic Design',
+		title: 'Eye catching designs that will engage your audience.',
+		desc:
+			'Custom images and videos help to captivate your audience and convey meaning. We can incorporate original designs to your website, social media, or marketing materials.'
+	}
+];
+
 const Services = () => {
+	const [ index, setIndex ] = useState(0);
+
+	const handleSelect = (selectIndex, e) => {
+		setIndex(selectIndex);
+	};
+
 	return (
 		<section id="site-section">
 			<div className="row mb-5">
@@ -19,50 +52,33 @@ const Services = () => {
 				</div>
 			</div>
 			<Container className="services-carousel">
-				<Carousel className="servies-carousel-image">
-					{images.map((image) => (
+				<Carousel
+					className="servies-carousel-image"
+					activeIndex={index}
+					onSelect={handleSelect}
+					controls={false}
+					indicators={false}
+				>
+					{images.map((img) => (
 						<Carousel.Item>
-							<img className="carousel-img d-block w-100" src={image} alt="First slide" />
+							<img className="carousel-img d-block w-100" src={img} alt="carousel" />
 						</Carousel.Item>
 					))}
 				</Carousel>
-				<Carousel className="services-carousel-text-item">
-					<Carousel.Item className="services-carousel-text-item">
-						<h2 className="section-title mb-3 text-primary h2">Custom Websites</h2>
-						<p className="lead">Design and build a unique website.</p>
-						<p>
-							We can create a website from scratch, or improve an existing website. We can also work with
-							templates and sites such as wordpress and wix.
-						</p>
-					</Carousel.Item>
-					<Carousel.Item className="services-carousel-text-item">
-						<h2 className="section-title mb-3 text-primary h2">Social Media Management</h2>
-						<p className="lead">Expand your outreach through social media.</p>
-						<p>
-							We can manage multiple social media accounts, handling everything from account setup to
-							continual maintenance. We will post content daily and will create graphic content and videos
-							on a monthly basis.
-						</p>
-					</Carousel.Item>
-					<Carousel.Item className="services-carousel-text-item">
-						<h2 className="section-title mb-3 text-primary h2">Digital Marketing</h2>
-						<p className="lead">
-							Vast array of tools and knowledge to help you achieve your marketing needs.
-						</p>
-						<p>
-							Search Engine Optimization, Email campaigns, advertising, are just a few options available
-							in our marketing arsenal. We can create innovative designs and content that will catch the
-							eye of your audience, and will increase your brand engagement.{' '}
-						</p>
-					</Carousel.Item>
-					<Carousel.Item className="services-carousel-text-item">
-						<h2 className="section-title mb-3 text-primary h2">Graphic Design</h2>
-						<p className="lead">Eye catching designs that will engage your audience.</p>
-						<p>
-							Custom images and videos help to captivate your audience and convey meaning. We can
-							incorporate original designs to your website, social media, or marketing materials.
-						</p>
-					</Carousel.Item>
+				<Carousel
+					className="services-carousel-text-item"
+					activeIndex={index}
+					onSelect={handleSelect}
+					controls={false}
+					indicators={false}
+				>
+					{content.map((c) => (
+						<Carousel.Item className="services-carousel-text-item">
+							<h2 className="section-title mb-3 text-primary h2">{c.header}</h2>
+							<p className="lead">{c.title}</p>
+							<p>{c.desc}</p>
+						</Carousel.Item>
+					))}
 				</Carousel>
 			</Container>
 		</section>
